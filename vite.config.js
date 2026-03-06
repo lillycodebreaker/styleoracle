b6385be14921e6b017f094c80078ee9deb1eb9ef
qi tan <lilyqitan@gmail.com>
2026-03-05 23:27:29 -0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.anthropic.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('error', (err) => console.log('proxy error', err));
+          proxy.on('proxyReq', (_, req) => console.log('proxying:', req.method, req.url));
+          proxy.on('proxyRes', (res) => console.log('proxy response:', res.statusCode));
+        }
+      }
+    }
+  }
 })
